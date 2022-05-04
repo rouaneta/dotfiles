b6e95494ded53d4d442b3ca27ab57c3db2465946
Arthur Rouanet <arthur.rouanet@lifen.fr>
2022-05-04 08:41:50 +0200

    fixup! Updates install script

--- install.sh
+++ install.sh
@@ -23,19 +23,19 @@ for name in *; do
   fi
 done
 
-REGULAR="\\033[0;39m"
-YELLOW="\\033[1;33m"
-GREEN="\\033[1;32m"
+# REGULAR="\\033[0;39m"
+# YELLOW="\\033[1;33m"
+# GREEN="\\033[1;32m"
 
 # zsh plugins
-CURRENT_DIR=`pwd`
-ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
-mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
-if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
-  echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'..."
-  git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
-fi
-cd "$CURRENT_DIR"
+# CURRENT_DIR=`pwd`
+# ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
+# mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
+# if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
+#   echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'..."
+#   git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+# fi
+# cd "$CURRENT_DIR"
 
 # setopt nocasematch
 # if [[ ! `uname` =~ "darwin" ]]; then
